dc9cf79afac6b71da314d1b0328053019f821fa5
Moises Angeles <moises.ea.camilo@gmail.com>
2026-05-22 20:53:56 -0400

    added config and middleware

--- main.go
+++ main.go
@@ -3,7 +3,12 @@ package main
 import (
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits	atomic.Int32
+}
 
 func readinessHandler(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
@@ -17,9 +22,15 @@ func main(){
 	const filepathRoot = "."
 	const port = "8080"
 	
+	cfg := apiConfig{}
+	handlerFileServer := http.FileServer(http.Dir(filepathRoot))
+	
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.Handle("/app/", http.StripPrefix("/app", cfg.middlewareMetricsInc(handlerFileServer)))
+	
 	mux.HandleFunc("/healthz", readinessHandler)
+	mux.HandleFunc("/metrics", cfg.serverHitsHandler)
+	mux.HandleFunc("/reset", cfg.resetHitsHandler)
 
 	srv := http.Server{
 		Addr: ":" + port,
